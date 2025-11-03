6a545e0549493bf9b00c2b4dcda3c6b5895c47d2
NÍCOLLAS GABRIEL BARTKO DE FRANÇA <nicollas_franca@estudante.sesisenai.org.br>
2025-11-03 19:23:33 -0300

    feat: incia estrutura do projeto

--- src/main/java/org/example/infrastructure/Conexao.java
+++ src/main/java/org/example/infrastructure/Conexao.java
@@ -1,4 +1,9 @@
-package org.example.infrastructure.repository;
+package org.example.infrastructure;
 
 public class Conexao {
+    private final static String URL = "";
+    private final static String USER = "";
+    private final static String SENHA = "";
+
+    public
 }
