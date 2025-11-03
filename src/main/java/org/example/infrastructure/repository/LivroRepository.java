57da8e1d07336199ead838eab8c810c7e5285c16
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2025-11-03 20:06:42 -0300

    feat: metodo cadastrarLivro na repositry

--- src/main/java/org/example/infrastructure/repository/LivroRepository.java
+++ src/main/java/org/example/infrastructure/repository/LivroRepository.java
@@ -1,7 +1,10 @@
 package org.example.infrastructure.repository;
 
+import org.example.infrastructure.Conexao;
 import org.example.model.Livro;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,7 +12,16 @@ import java.util.List;
 public class LivroRepository {
 
     public void cadastrarLivro(Livro livro) throws SQLException{
+        String query = "INSERT INTO livro(titulo, autor, ano) VALUES (?,?,?,?)";
 
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, livro.getTitulo());
+            stmt.setString(2, livro.getAutor());
+            stmt.setInt(3, livro.getAno());
+
+            stmt.executeUpdate();
+        }
     }
 
     public List<Livro> consultarTodosLivros() throws SQLException{
