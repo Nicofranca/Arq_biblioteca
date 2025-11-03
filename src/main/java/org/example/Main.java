477f9e1c4f02826e8d13f70fb930a20f36e72f9c
Nicofranca <nicollas_franca@estudante.sesisenai.org.br>
2025-11-03 20:35:05 -0300

    feat: cadastrar Livro service

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
+import org.example.service.EmprestimoService;
+import org.example.service.LivroService;
+import org.example.view.BibliotecaView;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        BibliotecaView bibliotecaView = new BibliotecaView();
+        LivroService livroService = new LivroService();
+        EmprestimoService emprestimoService = new EmprestimoService();
+
+        int escolha = 0;
+
+        do{
+            escolha = bibliotecaView.mostrarMenu();
+            switch (escolha){
+                case 1 -> livroService.cadastrarLivro();
+                case 2 -> emprestimoService.registrarEmprestimo();
+                case 3 -> emprestimoService.registrarDevolucao();
+                case 4 -> livroService.consultarLivrosCadastrados();
+                case 0 -> {
+                    return;
+                }
+            }
+        } while (escolha != 0);
     }
 }
--- src/main/java/org/example/view/BibliotecaView.java
+++ src/main/java/org/example/view/BibliotecaView.java
@@ -1,19 +1,39 @@
 package org.example.view;
 
+import org.example.model.Livro;
+import org.example.service.EmprestimoService;
+import org.example.service.LivroService;
+
 import java.util.Scanner;
 
 public class BibliotecaView {
     private Scanner sc = new Scanner(System.in);
+    private LivroService livroService = new LivroService();
+    private EmprestimoService emprestimoService = new EmprestimoService();
 
-    public BibliotecaView(Scanner sc) {
+    public BibliotecaView(Scanner sc, LivroService livroService, EmprestimoService emprestimoService) {
         this.sc = sc;
+        this.livroService = livroService;
+        this.emprestimoService = emprestimoService;
     }
 
     public BibliotecaView() {
+
     }
 
     public int mostrarMenu(){
-        int escolha = 0;
+        System.out.println("""
+                1 - Cadastrar Livro
+                2 - Cadastrar Emprestimo
+                3 - Devolucao de Livro
+                4 - Consultar Livros
+                5 - Consultar Emprestimos
+                """);
+        return capturarOpcao();
+    }
+
+    public int capturarOpcao(){
+        int escolha = sc.nextInt();
 
         return escolha;
     }
