eb94298650a1578039d5fe1f48fe9abcbf86f16e
NÍCOLLAS GABRIEL BARTKO DE FRANÇA <nicollas_franca@estudante.sesisenai.org.br>
2025-11-03 19:36:31 -0300

    feat: Biblioteca view

--- src/main/java/org/example/service/LivroService.java
+++ src/main/java/org/example/service/LivroService.java
@@ -1,4 +1,4 @@
 package org.example.service;
 
-public class LivrosService {
+public class LivroService {
 }
---
+++ src/main/java/org/example/view/BibliotecaView.java
@@ -0,0 +1,20 @@
+package org.example.view;
+
+import java.util.Scanner;
+
+public class BibliotecaView {
+    private Scanner sc = new Scanner(System.in);
+
+    public BibliotecaView(Scanner sc) {
+        this.sc = sc;
+    }
+
+    public BibliotecaView() {
+    }
+
+    public int mostrarMenu(){
+        int escolha = 0;
+
+        return escolha;
+    }
+}
