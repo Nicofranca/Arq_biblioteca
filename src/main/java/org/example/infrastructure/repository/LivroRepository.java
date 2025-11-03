b4b597c4c9a8e88c11bc96dbceaea7015a9f3200
NÍCOLLAS GABRIEL BARTKO DE FRANÇA <nicollas_franca@estudante.sesisenai.org.br>
2025-11-03 19:56:18 -0300

    feat: metodos LivroRepository

--- src/main/java/org/example/infrastructure/repository/LivroRepository.java
+++ src/main/java/org/example/infrastructure/repository/LivroRepository.java
@@ -1,6 +1,25 @@
 package org.example.infrastructure.repository;
 
+import org.example.model.Livro;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class LivroRepository {
 
+    public void cadastrarLivro(Livro livro) throws SQLException{
+
+    }
+
+    public List<Livro> consultarTodosLivros(){
+        List<Livro> listarLivros = new ArrayList<>();
+
+        return listarLivros;
+    }
+
+    public void atualizarStatusLivro(){
+
+    }
 
 }
