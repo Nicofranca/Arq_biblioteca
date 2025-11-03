fe8a9f3106a666736dc5a8be1bbeada0aae80dff
NÍCOLLAS GABRIEL BARTKO DE FRANÇA <nicollas_franca@estudante.sesisenai.org.br>
2025-11-03 19:27:17 -0300

    feat: Conexao

--- src/main/java/org/example/infrastructure/Conexao.java
+++ src/main/java/org/example/infrastructure/Conexao.java
@@ -1,9 +1,15 @@
 package org.example.infrastructure;
 
-public class Conexao {
-    private final static String URL = "";
-    private final static String USER = "";
-    private final static String SENHA = "";
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-    public
+public class Conexao {
+    private final static String URL = "jdbc:mysql://localhost:3306/biblioteca_db?useSSL=false&serverTimezone=UTC";
+    private final static String USER = "root";
+    private final static String SENHA = "mysqlPW";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, SENHA);
+    }
 }
